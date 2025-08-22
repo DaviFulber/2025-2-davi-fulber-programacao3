a5d6abb0d8a8ecf8575e5df634f6581ff1dff116
DaviFulber <daviagustofulber9@gmail.com>
2025-08-21 22:04:17 -0300

    AAAAA

---
+++ JAVASCRIPT/js_tag_semantica.js
@@ -0,0 +1,8 @@
+function atualizaData(){
+            const dataAgora = new Date();
+            const act = dataAgora.toLocaleDateString("pt-BR");
+            document.getElementById("data").innerHTML = "       " + act;
+
+
+        }
+        setInterval(atualizaData,1000);
